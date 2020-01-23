109601c0b256b5ccb0135999bc13f8fc6ad06a3b
Vincent Riou <vincent@invizbox.com>
2020-01-23 11:44:30 +0000

    InvizBox 2 Firmware Version 0.0.41

--- resources/clone_or_update.bash
+++ resources/clone_or_update.bash
@@ -4,27 +4,27 @@ set -e
 set -u
 
 folder=$1
-commit_SHA="9af5fb2fc2750e5b32ad8e603383c577a0682e5f"
+commit_SHA="3cd9353540b2d488692b2dde25d6a649409e77d0"
 repository="https://github.com/invizbox/openwrt.git"
 
 if [[ -d ${folder} ]]; then
     echo "A ${folder} repository already exists, checking if it is the correct one"
-    cd "${folder}"
+    cd ${folder}
     local_repo=$(git remote -v | grep origin | grep fetch | awk '{print $2}')
-    if [[ "${repository}" == "${local_repo}" ]]; then
+    if [[ ${repository} == ${local_repo} ]]; then
         echo "The repo is the correct one, updating it"
         git fetch --all -p
-        git checkout --force "${commit_SHA}" 2>/dev/null
+        git checkout --force ${commit_SHA} 2>/dev/null
         exit 0
     else
         echo "The repo wasn't the correct one. Expected [${repository}] but got [${local_repo}] removing it"
         cd ..
-        rm -rf "${folder}"
+        rm -rf ${folder}
     fi
 fi
 # now we either removed a repo that wasn't the correct one or none existed.
 echo "Cloning ${repository}"
-git clone "${repository}" "${folder}" 2>/dev/null
-cd "${folder}"
-git checkout "${commit_SHA}" 2>/dev/null
+git clone ${repository} ${folder} 2>/dev/null
+cd ${folder}
+git checkout ${commit_SHA} 2>/dev/null
 echo "Successful update/clone of repo ${repository}"
